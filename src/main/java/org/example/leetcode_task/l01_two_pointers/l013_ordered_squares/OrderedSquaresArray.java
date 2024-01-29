9ac56cb39d95f0036a1a312967d5c9f93149a002
AlexEmlnv <alex17prog@gmail.com>
2024-01-29 18:05:25 +0300

    Add task Ordered Squares Array

---
+++ src/main/java/org/example/leetcode_task/l01_two_pointers/l013_ordered_squares/OrderedSquaresArray.java
@@ -0,0 +1,36 @@
+package org.example.leetcode_task.l01_two_pointers.l013_ordered_squares;
+
+import java.util.Arrays;
+
+// time complexity O(n)
+// memory complexity O(n)
+public class OrderedSquaresArray {
+    public int[] makeOrderedSquares (int[] orig) {
+        int arrSize = orig.length;
+        if (arrSize == 0) return new int[0];
+
+        int left = 0;
+        int right = arrSize -1;
+        int index = arrSize -1;
+        int[] res = new int[arrSize];
+
+        while (left <= right) {  // 0 <= 0
+            if (orig[left] * orig[left] <  orig[right] * orig[right]) {
+                res[index] = orig[right] * orig[right];
+                right--;
+            } else {
+                res[index] = orig[left] * orig[left];
+                left++;
+            }
+            index--;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        OrderedSquaresArray array = new OrderedSquaresArray();
+        System.out.println(Arrays.toString(array.makeOrderedSquares(new int[]{-19, -2, 1, 2, 9})));
+        System.out.println(Arrays.toString(array.makeOrderedSquares(new int[]{1})));
+    }
+
+}
---
+++ src/test/java/org/example/leetcode_task/l01_two_pointers/l013_ordered_squares/OrderedSquaresArrayTest.java
@@ -0,0 +1,7 @@
+package org.example.leetcode_task.l01_two_pointers.l013_ordered_squares;
+
+import static org.junit.Assert.*;
+
+public class OrderedSquaresArrayTest {
+
+}
